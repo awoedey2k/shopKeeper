a25e76844167025b0da45b87fa52907380f47650
Ogunseye Olanrewaju <awoedey2k@gmail.com>
2015-07-21 09:02:51 +0100

    Application startup with config class

--- src/main/java/ng/com/cc/app/Application.java
+++ src/main/java/ng/com/cc/app/Application.java
@@ -6,14 +6,18 @@ import javax.sql.DataSource;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
 import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
 import org.springframework.web.servlet.view.tiles3.TilesView;
@@ -22,7 +26,16 @@ import org.springframework.web.servlet.view.tiles3.TilesView;
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
+@EnableConfigurationProperties(Application.class)
+@EnableScheduling
+// @ImportResource({ "classpath:*notification-context.xml", "classpath:*notification-si-context.xml",
+// "classpath:*notification-task-context.xml" })
+@EnableBatchProcessing
+// @PropertySource(value = { "file:${REMITA_HOME}/config/PaymentNotificationEngineapp.properties" })
+@PropertySource(value = { "file:C:/Users/LANRE/AppData/Roaming/Skype/My Skype Received Files/ShopKeeper/shopKeeper/src/main/java/application.properties" })
 public class Application {
+
+    private final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(this.getClass());
 
     @Value("${spring.datasource.driver-class-name}")
     String driverClassName;
@@ -41,10 +54,10 @@ public class Application {
 
     @Value("${tiles.layout}")
     private String tilesLayout;
-    
+
     @Value("${rootPath}")
     String rootPath;
-    
+
     @Value("${resources}")
     String resources;
 
@@ -54,15 +67,18 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(Application.class);
         ApplicationContext ctx = application.run(args);
-        LOG.debug("Let's inspect the beans provided by Spring Boot:");
+        Application.LOG.debug("Let's inspect the beans provided by Spring Boot:");
         String[] beanNames = ctx.getBeanDefinitionNames();
         Arrays.sort(beanNames);
-        LOG.debug(Arrays.toString(beanNames));
+        Application.LOG.debug(Arrays.toString(beanNames));
     }
 
 
     @Bean
     public UrlBasedViewResolver viewResolver() {
+        log.debug("Doing URL viewResolver>>>>>>>>>>>>>>>>>>>>>>>>>>>> ");
+        log.info("Doing URL viewResolver>>>>>>>>>>>>>>>>>>>>>>>>>>>> ");
+        log.error("Doing URL viewResolver>>>>>>>>>>>>>>>>>>>>>>>>>>>> ");
         UrlBasedViewResolver viewResolver = new UrlBasedViewResolver();
         viewResolver.setViewClass(TilesView.class);
         viewResolver.setOrder(1);
@@ -75,7 +91,7 @@ public class Application {
         TilesConfigurer tilesConfigurer = new TilesConfigurer();
         tilesConfigurer.setDefinitions(new String[] { tilesBaseLayout, tilesLayout });
         tilesConfigurer.setCompleteAutoload(false);
-        LOG.info("tiles configurer in place...: " + tilesConfigurer);
+        Application.LOG.info("tiles configurer in place...: " + tilesConfigurer);
         return tilesConfigurer;
     }
 
